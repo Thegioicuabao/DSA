68c2937a36d10f25c57bf5cbec0d9d1ae135aea0
thegioicuabao <vickyvuvo@gmail.com>
2020-01-01 18:51:23 +0700

    quick sort

--- sort/BubbleSort.cpp
+++ sort/BubbleSort.cpp
@@ -23,7 +23,7 @@ void selectionSort(int a[], int n){
     swap(a[vtmin], a[i])
 }
 
-void insertionSort(int a[], int n){
+void insertionSort(int a[], int n) {
   int x,pos;
   for(int i=1; i<n; i++){
     x=a[i], pos=i-1;
@@ -34,4 +34,41 @@ void insertionSort(int a[], int n){
     }
     a[pos+1] = x;
   }
+}
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        { 
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
